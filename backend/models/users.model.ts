b1da2c88e434ff7daf26e6a0ddf7ef4e0f06d919
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-11 13:11:09 +0000

    Feat: Separated users model into separate file

---
+++ backend/models/users.model.ts
@@ -0,0 +1,93 @@
+import postgres from "postgres";
+
+import sql from "../helpers/dbconnection";
+import { User } from "../interfaces/users";
+
+export const update = async (
+  userId: number,
+  user: User
+): Promise<postgres.RowList<postgres.Row[]>> => {
+  try { 
+    const users = sql`update users set ${sql(
+      user,
+      "firstname",
+      "lastname",
+      "email",
+      "password",
+      "isadmin"
+    )} where id = ${userId}`;
+    return await users;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+export const getAll = async (): Promise<postgres.RowList<postgres.Row[]>> => {
+  try {
+    const users = sql`SELECT id,firstname,lastname,email,isadmin FROM users`;
+    return await users;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+export const get = async (id: number) : Promise<postgres.RowList<postgres.Row[]>> => {
+    try {
+        const user = sql`SELECT id,firstname,lastname,email,isadmin FROM users WHERE id = ${id}`;
+        return await user;
+    }catch(error: any) {
+        throw error;
+    }
+}
+export const add = async (user: User) : Promise<postgres.RowList<postgres.Row[]>> => {
+    try {         
+        const users = sql`INSERT INTO users ${sql(user, 'firstname', 'lastname', 'email','password', 'isadmin')}`; 
+        return await users; 
+    }catch(error: any) {
+        throw error;
+    }
+}
+export const drop = async (userId: number) : Promise<postgres.RowList<postgres.Row[]>> => {
+    try {    
+        const users = sql`delete from users where id = ${userId}`;
+        return await users;
+    }catch(error: any) {
+        throw error;
+    }
+}
+
+export const getIsAdminByUserId = async (userId: number) : Promise<postgres.RowList<postgres.Row[]>> => {
+    try {
+        const getSql = sql`SELECT isadmin FROM users WHERE id = ${userId}`;
+        return await getSql;
+    }catch(error) {
+        throw error;
+    }
+}
+export const getIdByEmail = async (email: string) : Promise<postgres.RowList<postgres.Row[]>> => {
+    try {
+        const users = sql`SELECT id FROM users where email = ${email}`; 
+        return await users; 
+    }catch(error) {
+        throw error;
+    }
+}
+
+export const getUserByEmail = async (email: string) : Promise<postgres.RowList<postgres.Row[]>> => {
+  try {
+      const user = sql`SELECT id,firstname,lastname,email,isadmin FROM users WHERE email = ${email}`;
+      return await user;
+  }catch(error) {
+      throw error;
+  }
+}
+export const modifyPassword = async (newPassword: string, id: number) : Promise<postgres.RowList<postgres.Row[]>> => {
+  try {
+      const user = sql`update users set password = ${newPassword} where id = ${id}`;
+      return await user;
+  }catch(error) {
+      throw error;
+  }
+}
+
+
