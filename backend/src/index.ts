9a32b76703817bcb31671fe6b37e72a8b03ca547
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-13 18:12:22 +0000

    Feat: Added self-signed certs to server.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import passport from "./middlewares/passport.middleware"; 
 import helmet from "helmet";
-import cors from 'cors'
+import cors from 'cors';
+import fs from 'fs';
+import path from "path";
+import https from "https";
 
 import userRouter from "./routes/users.router";
 import eventsRouter from "./routes/events.router";
 import authRouter from "./routes/auth.router"; 
-import cookieSessionMiddleware from "./middlewares/cookiesession.middleware";
+import {cookieSessionMiddleware, cookieSessionBugFix} from "./middlewares/cookiesession.middleware";
+import isauthenticated from "./middlewares/isauthenticated.middleware";
 
 let { PORT } = process.env;
 
@@ -16,6 +20,7 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(cookieSessionMiddleware);
+app.use(cookieSessionBugFix);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -24,9 +29,12 @@ app.get("/", (req, res) => {
 });
 
 app.use('/auth', authRouter);
-app.use('/users', userRouter);
+app.use('/users', passport.authenticate('bearer', { session: false }), isauthenticated, userRouter);
 app.use('/events', eventsRouter);
 
-app.listen(PORT, () => {
+https.createServer({
+  key: fs.readFileSync(path.resolve(__dirname, '../key.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname, '../cert.pem'))
+}, app).listen(PORT, () => {
   console.log(`Connected to port ${PORT}`);
 });
