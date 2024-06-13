d8b2005c24ea31c0e7ddee8c7dcd309ac813f1ec
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-13 04:27:46 +0000

    Feat: Added additional routes to logout user

--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -1,11 +1,26 @@
 import express from 'express';
-import { loginUser, registerUser, resetPassword, verifyUser } from '../controllers/auth.controller';
+import { loginUser, registerUser, resetPassword, verifyUser, authFailure, authSuccess } from '../controllers/auth.controller';
+import passport from 'passport';
 
 const authRouter = express.Router();
 
-authRouter.post('/login', loginUser)
-authRouter.post('/register', registerUser)
-authRouter.post('/reset-password/:id', resetPassword)
-authRouter.post('/verify', verifyUser)
+authRouter.post('/', authSuccess);
+authRouter.post('/login', loginUser);
+authRouter.post('/register', registerUser);
+authRouter.post('/reset-password/:id', resetPassword);
+authRouter.post('/verify', verifyUser);
+authRouter.get('/google', passport.authenticate('google', {
+    scope: ['email', 'profile']
+}));
+authRouter.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: '/auth/failure',
+    successRedirect: '/',
+    session: true
+}));
+authRouter.get('/logout', (req : any, res : any) => {
+    req.logout();
+    res.json({isSuccess: true, message: 'User logged out successfully.'});
+});
+authRouter.get('/failure', authFailure);
 
 export default authRouter;
