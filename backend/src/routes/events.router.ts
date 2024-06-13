87058ae81825ea11095394277fffeaf99b8f0402
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-13 04:26:59 +0000

    Enhancement: Added auth routes to protected routes

--- backend/src/routes/events.router.ts
+++ backend/src/routes/events.router.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { addEvent, deleteEvent, getEvent, getEvents, getLoggedInUserEvents, modifyEvent } from '../controllers/events.controller';
+import isauthenticated from '../middlewares/isauthenticated.middleware';
 
 const eventsRouter = express.Router();
 
 eventsRouter.get('/', getLoggedInUserEvents);
 eventsRouter.get('/all', getEvents);
 eventsRouter.get('/:id', getEvent);
-eventsRouter.post('/', addEvent);
-eventsRouter.put('/:id', modifyEvent);
-eventsRouter.delete('/:id', deleteEvent);
+eventsRouter.post('/', isauthenticated, addEvent);
+eventsRouter.put('/:id', isauthenticated, modifyEvent);
+eventsRouter.delete('/:id', isauthenticated, deleteEvent);
 
 export default eventsRouter;
