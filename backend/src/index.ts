e176b8b2af536cba1d89e53dc696066b4eb6271c
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-13 04:29:24 +0000

    Feat: Registered middleware routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,32 @@
 import express from "express";
-import cors from "cors";  
-import 'dotenv/config'; 
+import passport from "./middlewares/passport.middleware"; 
+import helmet from "helmet";
+import cors from 'cors'
 
 import userRouter from "./routes/users.router";
 import eventsRouter from "./routes/events.router";
-import authRouter from "./routes/auth.router";
+import authRouter from "./routes/auth.router"; 
+import cookieSessionMiddleware from "./middlewares/cookiesession.middleware";
 
-let { PORT } = process.env; 
+let { PORT } = process.env;
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(cookieSessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => { 
     res.json("Welcome to Events Manager System API");
 });
 
-app.use('/auth', authRouter)
+app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/events', eventsRouter);
 
-
 app.listen(PORT, () => {
-  console.log("Connected to port 8800");
+  console.log(`Connected to port ${PORT}`);
 });
