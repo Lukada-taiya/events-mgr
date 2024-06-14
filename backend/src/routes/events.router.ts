f5e1e110466cd5e68b3a203ab970f6fad5e46cfd
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-14 11:48:49 +0000

    Added authentication guard to events routes

--- backend/src/routes/events.router.ts
+++ backend/src/routes/events.router.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import { addEvent, deleteEvent, getEvent, getEvents, getLoggedInUserEvents, modifyEvent } from '../controllers/events.controller';
 import isauthenticated from '../middlewares/isauthenticated.middleware';
+import passport from 'passport';
 
 const eventsRouter = express.Router();
 
 eventsRouter.get('/', getLoggedInUserEvents);
 eventsRouter.get('/all', getEvents);
 eventsRouter.get('/:id', getEvent);
-eventsRouter.post('/', isauthenticated, addEvent);
-eventsRouter.put('/:id', isauthenticated, modifyEvent);
-eventsRouter.delete('/:id', isauthenticated, deleteEvent);
+eventsRouter.post('/', passport.authenticate('bearer', { session: false }),isauthenticated, addEvent);
+eventsRouter.put('/:id',passport.authenticate('bearer', { session: false }), isauthenticated, modifyEvent);
+eventsRouter.delete('/:id',passport.authenticate('bearer', { session: false }), isauthenticated, deleteEvent);
 
 export default eventsRouter;
